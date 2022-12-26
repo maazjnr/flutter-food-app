c28bfdeea1f93327957667b9312f1e2d1d2e43d0
maazjnr <goalmaaz@gmail.com>
2022-12-26 03:24:18 -0800

    statefull

---
+++ lib/homepage.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var icons = CupertinoIcons.heart;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomePage'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: IconButton(
+            iconSize: 150,
+            onPressed: (() {
+              setState(
+                () {
+                  icons = CupertinoIcons.heart_fill;
+                },
+              );
+            }),
+            icon: Icon(icons, color: Colors.red),),
+
+      ),
+    );
+  }
+}
